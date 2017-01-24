2c3020eaa623f20c9f989d4f60b94d3c93af8ec2
Nacho Codoñer <igcogi@gmail.com>
2017-01-24 12:19:12 +0100

    prepare package

---
+++ package.js
@@ -0,0 +1,10 @@
+Package.describe({
+  name: 'pathable-vendor',
+  version: '0.0.1',
+  documentation: 'README.md',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.4.2.3');
+  api.use('ecmascript');
+});
