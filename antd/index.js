2174a05a34cf2b664e3bcaa53573c0e605a625ab
Ivan Jovanovic <ivanjov96@gmail.com>
2017-04-17 20:15:20 +0200

    Added select antd component

--- antd/index.js
+++ antd/index.js
@@ -1,3 +1,4 @@
 export { DatePicker } from 'antd/lib/date-picker';
 export { TimePicker } from 'antd/lib/time-picker';
 export { LocaleProvider } from 'antd/lib/locale-provider';
+export { Select } from 'antd/lib/select';
